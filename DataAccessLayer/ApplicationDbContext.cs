7a8289374e940dc51daff9ada78f16a915aae3cb
Aliaksandr Shakavets <Aliaksandr_Shakavets@epam.com>
2019-12-29 13:27:53 +0300

    Initialise .Net solution. Add Data Access Layer project. Add Location Model

---
+++ DataAccessLayer/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccessLayer
+{
+    /// <summary>
+    /// Represents an application database context.
+    /// </summary>
+    public class ApplicationDbContext : DbContext
+    {
+        public virtual DbSet<Location> Locations { get; set; }
+        public ApplicationDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json");
+            var configure = builder.Build();
+
+            optionsBuilder.UseSqlServer(configure.GetConnectionString("DefaultConnection"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new LocationConfiguration());
+        }
+    }
+}
---
+++ DataAccessLayer/Model/LocationConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccessLayer.Models
+{
+    public class LocationConfiguration : IEntityTypeConfiguration<Location>
+    {
+        public void Configure(EntityTypeBuilder<Location> builder)
+        {
+            builder.HasKey(i => i.Id);
+            builder.HasAlternateKey(i => i.Address);
+            builder.Property(i => i.Name).HasColumnName("Name").HasMaxLength(60).IsRequired();
+            builder.Property(i => i.Address).HasColumnName("Email_address").HasMaxLength(60).IsRequired();
+        }
+    }
+}
