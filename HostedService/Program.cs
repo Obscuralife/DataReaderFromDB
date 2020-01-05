e17ab2d13802f862cb3d18a05754ef50f6842ec6
Aliaksandr Shakavets <Aliaksandr_Shakavets@epam.com>
2020-01-05 07:00:57 +0300

    Add implementation hosted service. Add Nlog logger

---
+++ HostedService/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
+
+namespace HostedService
+{
+    /// <summary>
+    /// Represents a main host service class.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// /// Represents a main entry point to host service.
+        /// </summary>
+        /// <param name="args">.</param>
+        public static void Main(string[] args)
+        {
+            var logger = NLogBuilder.ConfigureNLog("NLog.config").GetCurrentClassLogger();
+
+            try
+            {
+                logger.Info(new string('*', Console.WindowWidth / 2));
+                logger.Info($"Application Starts. Version: {System.Reflection.Assembly.GetEntryAssembly()?.GetName().Version}");
+                logger.Info($"Application Directory: {AppDomain.CurrentDomain.BaseDirectory}");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                logger.Fatal(ex, "Application terminated unexpectedly");
+            }
+            finally
+            {
+                logger.Info(new string('*', Console.WindowWidth / 2) + "\r");
+                LogManager.Shutdown();
+            }
+        }
+
+        /// <summary>
+        /// Creates host builder.
+        /// </summary>
+        /// <param name="args">.</param>
+        /// <returns><see cref="IHostBuilder"/>.</returns>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                    services.Configure<AppSettings>(hostContext.Configuration.GetSection("AppSettings"));
+                })
+            .ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+            })
+            .UseNLog();
+    }
+}
---
+++ HostedService/Worker.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace HostedService
+{
+    /// <inheritdoc/>
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> logger;
+        private readonly string dataPath;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Worker"/> class.
+        /// </summary>
+        /// <param name="logger">.</param>
+        /// <param name="settings">..</param>
+        public Worker(ILogger<Worker> logger, IOptions<AppSettings> settings)
+        {
+            this.logger = logger;
+            dataPath = settings.Value.PathToLocationsFile;
+        }
+
+        /// <inheritdoc/>
+        public override void Dispose()
+        {
+            base.Dispose();
+        }
+
+        /// <inheritdoc/>
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Service Starting");
+            if (!File.Exists(dataPath))
+            {
+                logger.LogWarning($"Please make sure the input file [{dataPath}] exists, then restart the service.");
+                return Task.CompletedTask;
+            }
+
+            return base.StartAsync(cancellationToken);
+        }
+
+        /// <inheritdoc/>
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            return base.StopAsync(cancellationToken);
+        }
+
+        /// <inheritdoc/>
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
