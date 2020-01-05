74ece68fddd2c910f52506e54dff75af9f6252b2
Aliaksandr Shakavets <Aliaksandr_Shakavets@epam.com>
2020-01-05 07:01:46 +0300

    Step 7 - Add implementation hosted service. Add Nlog logger.

---
+++ HostedService/AppSettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HostedService
+{
+    /// <summary>
+    /// Represents application settings.
+    /// </summary>
+    public class AppSettings
+    {
+        /// <summary>
+        /// Gets or sets json data base file name.
+        /// </summary>
+        public string LocationsFileName { get; set; }
+
+        /// <summary>
+        /// Gets path to json data base.
+        /// </summary>
+        public string PathToLocationsFile { get => GetPath(); }
+
+        private string GetPath()
+        {
+            var baseDir = Environment.CurrentDirectory;
+            var rootFolder = baseDir.Remove(baseDir.IndexOf("HostedService"));
+            return Path.Combine(rootFolder, LocationsFileName);
+        }
+    }
+}
